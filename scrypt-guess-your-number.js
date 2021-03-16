ebc63ff1ad5b5a34a9207c8f6127d2032d2122f4
Петрович <71115406+Alkor7@users.noreply.github.com>
2021-03-16 22:42:38 +0300

    Add files via upload

--- scrypt-guess-your-number.js
+++ scrypt-guess-your-number.js
@@ -1,6 +1,11 @@
-var iMin=1, iMax=100, MyNum=0, newText;
+var iMin=1, iMax=100, MyNum=0, newText, btnState;
 
 function ugadator () {
+
+	console.info("Начинаем!");
+
+btnState = document.getElementById("ButtonStartJS");
+btnState.style.display = "none";
 
 	var rndNum = function (min, max) {
 		return Math.floor(Math.random() * (max - min + 1) + min);
@@ -12,30 +17,30 @@ function ugadator () {
 	newText.style.fontSize = "36px";
 	newText.innerText=(MyNum);
 
+	document.addEventListener('keyup', function(event) {
 
-document.addEventListener('keyup', function(event) {
+		if (event.key == "ArrowUp") {
+			iMin = (MyNum+1);
+			MyNum = rndNum(iMin, iMax);
+			console.log("Нажал Вверх. iMin=" + iMin + " iMax =" + iMax + " MyNum =" + MyNum);
+			newText = document.getElementById("changeBlock")
+			newText.textContent=(MyNum);
 
-	if (event.key == "ArrowUp") {
-		iMin = MyNum;		
-		MyNum = rndNum(iMin, iMax);
-		console.log("Нажал Вверх. Min=" + iMin + " Max =" + iMax + " Число =" + MyNum);
-		newText = document.getElementById("changeBlock")
-		newText.textContent=(MyNum);
+		}
+		if (event.key == "ArrowDown") {
+			iMax = (MyNum-1);
+			MyNum = rndNum(iMin, iMax);
+			console.info("Нажали Вниз. iMin=" + iMin + " iMax =" + iMax + " MyNum =" + MyNum);
+			newText = document.getElementById("changeBlock")
+			newText.textContent=(MyNum);
 
-	}
-	if (event.key == "ArrowDown") {
-		iMax = MyNum;
-		MyNum = rndNum(iMin, iMax);
-		console.info("Нажали Вниз. Min=" + iMin + " Max =" + iMax + " Число =" + MyNum);
-		newText = document.getElementById("changeBlock")
-		newText.textContent=(MyNum);
-		
-	}
-	if (event.key == "Enter") {
-		console.info("УГАДАЛ число = " + MyNum);
-		newText.style.backgroundColor=("#00FF00");
-		newText = document.getElementById("changeBlock")
-		newText.textContent=("Я угадал Ваше число = " + MyNum);
-	}
-});
+		}
+		if (event.key == "Enter") {
+			console.info("УГАДАЛ число = " + MyNum);
+			newText = document.getElementById("changeBlock")
+			newText.textContent=("Я угадал Ваше число = " + MyNum);
+			newText = document.getElementById("newTry");
+			newText.style.display = "block"
+		}
+	});
 }
